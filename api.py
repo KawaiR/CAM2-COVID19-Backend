ab81ebb2ff75a0fcd43f12a860fd199b3e4f390b
Xinglei Liu <liu2177@purdue.edu>
2020-11-02 21:17:36 -0500

    combined commit

--- api.py
+++ api.py
@@ -8,30 +8,28 @@ import mpld3
 from mpld3 import plugins
 import json
 import random
+import scatterplots
 
 # This function generates the plot
-def plot(start, end):
-  x = range(start, end)
-  y = range(start, end)
-  fig, ax = plt.subplots()
-  ax.scatter(x, y)
-  ax.set(title="Vehicle count from date " + str(start) + " to " + str(end))
-  fig.set_size_inches(2, 1.5)
-  return mpld3.fig_to_html(fig)
+def plot(place, start, end):
+  print(place)
+  return scatterplots.generate_plot(country=place, date1=start[5:]+" ", date2=end[5:]+" ")
 
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+  min_date = "2020-04-01"
+  max_date = "2020-10-25"
+  return render_template("index.html", min_date=min_date, max_date=max_date)
 
 # This function get the POST request and return the plot in format of html
 @app.route('/query', methods = ['POST'])
 def query():
   # Get data from ajax request
   data = json.loads(request.data)
-  plt_html = plot(int(data["start"]), int(data["end"]))
+  plt_html = plot(data["country"], data["start"], data["end"])
   return plt_html
 
 
